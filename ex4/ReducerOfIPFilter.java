5acf4ff93b51764130d2681f6b6a0db07b8bb50e
余一夏 <405563383@qq.com>
2016-11-03 16:23:22 +0800

    Update ReducerOfIPFilter.java

--- ex4/ReducerOfIPFilter.java
+++ ex4/ReducerOfIPFilter.java
@@ -1,12 +1,27 @@
 package No4;
 
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
+/**
+ * Created by Jacoulin on 2016/11/3.
+ * 
+ * 测试命令
+ * cat access.log | java -cp java5.jar com.jacoulin.MapperOfIPFilter "2015-12-31 18:00:00" "2015-12-31 19:00:00" | java -cp java5.jar com.jacoulin.ReducerFilter
+ * 
+ */
 public class ReducerOfIPFilter {
-	public static void main(String[] args){
-		Scanner scanner = new Scanner(System.in);
-		while (scanner.hasNext()){
-			String line = scanner.nextLine();
-		}
-	}
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        //使用set集合存放ip，保证没有重复ip地址，方便统计
+        Set<String> s = new HashSet<String>();
+        while (scanner.hasNext()){
+            String[] line = scanner.nextLine().split("\\t");
+            //将ip添加进Set集合
+            s.add(line[0]);
+        }
+        //直接输出集合ip的个数
+        System.out.println(s.size());
+    }
 }
